0f08c082097e1e703a36bc8592f923f4dcf64d63
akintayodaniel210-byte <akintayodaniel210@gmail.com>
2026-04-15 12:18:16 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function sendMessage() {
+    let name = document.getElementById("name").value;
+
+    if (name === "") {
+        alert("Please enter your name!");
+    } else {
+        alert("Thank you " + name + "! Your message has been sent😊.");
+    }
+}       alert("I Hope You can Enjoy Your Stay Here😊")
